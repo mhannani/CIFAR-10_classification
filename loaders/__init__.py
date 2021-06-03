5417e726cc3c12f71abf2bcb5557b9c220544e25
HANNANI Mohamed <49957450+mhannani@users.noreply.github.com>
2021-06-03 22:47:54 +0100

    Updating the README.md file

--- loaders/__init__.py
+++ loaders/__init__.py
@@ -1,8 +1,10 @@
 from .load_cifar_10 import load_cifar_10
+from .load_model import load_model
 
 NUM_CLASSES = 10
 
 __all__ = [
     'load_cifar_10',
-    'NUM_CLASSES'
+    'NUM_CLASSES',
+    'load_model'
 ]
